84d1d3e20f6252a8507bfa24feb5e4aafc525427
Jeremy Johnstone <jstone@air-33j-net.local>
2009-07-04 05:00:49 -0700

    moving the last two form fields into Ruby

--- polynome/app/controllers/jobs_controller.rb
+++ polynome/app/controllers/jobs_controller.rb
@@ -1,6 +1,10 @@
 class JobsController < ApplicationController
   def index
-    @job = Job.new(:nodes => 3);
+    @job = Job.new(:nodes => 3, :input_data => 
+"1.2  2.3  3.4
+1.1  1.2  1.3
+2.2  2.3  2.4
+0.1  0.2  0.3");
   end
   def generate
     @job = Job.new(params[:job]);
