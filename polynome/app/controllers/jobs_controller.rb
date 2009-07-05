9dd52599e725a78aa3faba1fadba1b68412254f0
Jeremy Johnstone <jstone@air-33j-net.local>
2009-07-05 16:14:59 -0700

    removing duplicate function definition which got accidentally added

--- polynome/app/controllers/jobs_controller.rb
+++ polynome/app/controllers/jobs_controller.rb
@@ -91,6 +91,7 @@ class JobsController < ApplicationController
     macauley_opts[:post_m2_command] = "dot -T" + file_format + " -o " + graphfile + " " + dotfile;
     macauley2(macauley_opts);
   end
+  
   def minsets_generate_wiring_diagram(discretized_data_files, file_format, p_value, n_nodes)
     dotfile = "public/perl/" + @file_prefix + ".wiring-diagram.dot";
     graphfile = "public/perl/" + @file_prefix + ".wiring-diagram." + file_format;
@@ -103,18 +104,7 @@ class JobsController < ApplicationController
     macauley_opts[:post_m2_command] = "dot -T" + file_format + " -o " + graphfile + " " + dotfile;
     macauley2(macauley_opts);
   end
-  def generate_wiring_diagram(discretized_data_files, file_format, p_value, n_nodes)
-    dotfile = "public/perl/" + @file_prefix + ".wiring-diagram.dot";
-    graphfile = "public/perl/" + @file_prefix + ".wiring-diagram." + file_format;
-    datafiles_string = make_m2_string_from_array(discretized_data_files);
-    
-    macauley_opts = {};
-    macauley_opts[:m2_command] = 'wd( ' + datafiles_string + ', \"../' + dotfile + 
-        '\",  ' + p_value + ', ' + n_nodes.to_s + ' )';
-    macauley_opts[:m2_file] = "wd.m2";
-    macauley_opts[:post_m2_command] = "dot -T" + file_format + " -o " + graphfile + " " + dotfile;
-    macauley2(macauley_opts);
-  end
+  
   def is_data_consistent(discretized_data_files, p_value, n_nodes)
     function_file= "public/perl/" + @file_prefix + ".consistent.txt";
     datafiles_string = make_m2_string_from_array(discretized_data_files);
@@ -125,6 +115,7 @@ class JobsController < ApplicationController
     macauley_opts[:m2_file] = "isConsistent.m2";
     return macauley2(macauley_opts);
   end
+  
   def sgfan(discretized_data_files, p_value, n_nodes)
     functionfile = "public/perl/" + @file_prefix + ".functionfile.txt";
     datafiles_string = make_m2_string_from_array(discretized_data_files);
@@ -135,6 +126,7 @@ class JobsController < ApplicationController
     macauley_opts[:m2_file] = "func.m2";
     macauley2(macauley_opts);
   end
+  
   def minsets(discretized_data_files, file_format, p_value, n_nodes)
     functionfile = "public/perl/" + @file_prefix + ".functionfile.txt";
     datafiles_string = make_m2_string_from_array(discretized_data_files);
