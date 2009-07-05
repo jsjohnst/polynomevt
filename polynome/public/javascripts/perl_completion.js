11f8917c7306f658c79832df87546fe1d635a24d
Jeremy Johnstone <jstone@air-33j-net.local>
2009-07-04 21:11:41 -0700

    adding in javascript which polls to check if the job is finished yet or not

---
+++ polynome/public/javascripts/perl_completion.js
@@ -0,0 +1,14 @@
+var done = 0;
+var htmlBody = document.getElementById("bodytag");
+
+function check_perl_completed(prefix) {
+	if(!done) {
+		var node = document.createElement("script");
+		node.src = "/perl/" + prefix + ".done.js?t=" + Date.now();
+		htmlBody.appendChild(node);
+		setTimeout("check_perl_completed('" + prefix + "')", 5000);
+	} else {
+		var node = document.getElementById("completion_msg");
+		node.innerHTML = 'Your data has been generated successfully!';
+	}
+}
