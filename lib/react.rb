d76a123af0060bef4fc11d8aa23bf5119e192898
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-07-15 23:52:33 -0400

    starting to write fileman.txt

--- lib/react.rb
+++ lib/react.rb
@@ -7,6 +7,31 @@ module React
   def parse_output(infile, outfile)
   end
 
+
+# n_nodes, file_prefix
   def write_manager_file
+    ## Needs to look like this 
+	## P=2;
+	## N=8;
+	## WT = {"w1.txt","w2.txt"};
+	## KO = {};
+	## REV = {};
+	## CMPLX = {};
+	## BIO = {};
+	## MODEL = {};
+	## PARAMS = {"params1.txt"};
+
+    filemanagerfile = "EA/" + @file_format +".fileman.txt";
+    data = "P=2;
+    N=#{n_nodes};
+    WT = {"w1.txt","w2.txt"};
+    KO = {};
+    REV = {};
+    CMPLX = {};
+    BIO = {};
+    MODEL = {};
+    PARAMS = {\"params.txt\"};"
+    File.open(filemanagerfile, 'w') { |file| file.write(data) }
+
   end
 end
