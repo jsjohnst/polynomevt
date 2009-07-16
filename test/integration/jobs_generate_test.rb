7d50a12ac836dfd0af47957dec82f1708819f274
Michael Stillman <mike@indigo.local>
2009-07-15 22:43:58 -0400

    added new directory and a test that doesn't yet work well

---
+++ test/integration/jobs_generate_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class JobsGenerateTest < ActionController::IntegrationTest
+  fixtures :jobs
+
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+  
+  def test_generate_discretization_only
+    get '/'
+    assert_response :success
+    post '/jobs/generate', :job => {
+      :show_discretized => true
+    }
+    assert_response :success
+    end
+end
