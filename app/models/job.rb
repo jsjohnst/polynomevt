2da187e232b4ec396d052346f82d1330695133ae
Jeremy Johnstone <github@jeremyjohnstone.com>
2009-07-22 22:18:48 -0700

    cleaning out old stuff as we are starting from scratch for version2

--- app/models/job.rb
+++
@@ -1,7 +0,0 @@
-class Job < ActiveRecord::Base
-  validates_presence_of :nodes
-  validates_numericality_of :nodes, :only_integer => true, :message => "Number of nodes must be an integer between 1 and 11"
-  validates_numericality_of :nodes, :less_than => 12, :message => "Number of nodes is too big!"
-  validates_numericality_of :nodes, :greater_than => 0, :message => "Number of nodes is too small!"
-
-end
