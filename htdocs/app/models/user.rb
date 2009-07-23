9cbc231d4aa27ce3361f922fb7d5377ef8f5ce7f
Jeremy Johnstone <github@jeremyjohnstone.com>
2009-07-23 02:01:28 -0700

    updated the scaffolded html to show the user name, not the ID and also relaxed the email validation to only insist on it being valid if provided, and prettified the html slightly on the jobs index

--- htdocs/app/models/user.rb
+++ htdocs/app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates_presence_of :login, :password
   validates_uniqueness_of :login, :email
   validates_confirmation_of :password
-	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
+	validates_format_of :email, :with => /(^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$)*/i, :message => "Invalid email"
 end
