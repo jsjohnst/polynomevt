22c5adbc0280b05ddd7d3e2475ccb0ee769fc9cf
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-07-23 06:23:53 -0400

    tests for valid users

--- htdocs/app/models/user.rb
+++ htdocs/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-	has_many :job
-	validates_length_of :login, :within => 3..40
+  has_many :job
+  validates_length_of :login, :within => 3..40
   validates_length_of :password, :within => 6..40
-  validates_presence_of :login, :password
+  validates_presence_of :login , :password
   validates_uniqueness_of :login, :email
   validates_confirmation_of :password
-	validates_format_of :email, :with => /(^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$)*/i, :message => "Invalid email"
+  validates_format_of :email, :with => /^(([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,}))*$/i, :message => "Invalid email"
 end
--- htdocs/test/functional/users_controller_test.rb
+++ htdocs/test/functional/users_controller_test.rb
@@ -12,9 +12,29 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not save invalid user" do 
+    my_user = users(:invalid_user_without_login)
+    assert !my_user.save, "should not save user without login"
+    my_user = users(:invalid_user_with_invalid_email)
+    assert !my_user.save, "should not save user with invalid email"
+  end 
+
+  test "should save simple user" do 
+    my_user = users(:valid_user)
+    assert my_user.save
+  end
+
+  test "should not allow duplicate email addresses" do 
+    my_user1 = users(:valid_user_with_double_email_address)
+    assert my_user1.save, "first user should have been saved" 
+    my_user2 = users(:valid_user_with_double_email_address)
+    assert !my_user2.save, "second user with same email address should not
+    have been saved"
+  end
+
   test "should create user" do
     assert_difference('User.count') do
-      post :create, :user => { :login => "user", :password => "fubarbaz" }
+      post :create, :user => { :login => "useronlywithpassword", :password => "fubarbaz" }
     end
 
     assert_redirected_to user_path(assigns(:user))
