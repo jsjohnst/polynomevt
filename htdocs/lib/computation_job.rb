8ae621fbd46163d7b295da0cdad5b2c893639b4a
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-07-23 02:57:50 -0400

    added to comment

--- htdocs/lib/computation_job.rb
+++ htdocs/lib/computation_job.rb
@@ -1,5 +1,6 @@
 class ComputationJob < Struct.new(:job_id)  
   def perform  
     # here is where we will handle the background Macaulay processing  
+    # and simulation of the network
   end  
 end
