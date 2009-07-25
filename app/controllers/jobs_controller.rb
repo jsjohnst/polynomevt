86134eac4b2b8aac9d56a3714166996f7fb98538
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-07-25 06:34:52 -0400

    removing superfluous semi-colons

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,15 +8,15 @@ class JobsController < ApplicationController
   layout "main"
 
   def functionfile_name(prefix)
-    "public/perl/" + prefix + ".functionfile.txt";
+    "public/perl/" + prefix + ".functionfile.txt"
   end
   
   def dotfile_name(prefix)
-    "public/perl/" + prefix + ".wiring-diagram.dot";
+    "public/perl/" + prefix + ".wiring-diagram.dot"
   end
     
   def graphfile_name(prefix, file_format)
-    "public/perl/" + prefix + ".wiring-diagram." + file_format;
+    "public/perl/" + prefix + ".wiring-diagram." + file_format
   end
 
   
@@ -26,85 +26,85 @@ class JobsController < ApplicationController
 1.2  2.3  3.4
 1.1  1.2  1.3
 2.2  2.3  2.4
-0.1  0.2  0.3");
+0.1  0.2  0.3")
 
-    @error_message = params[:error_message];
+    @error_message = params[:error_message]
   end
 
   def generate
     # Boolean, not multistate yet  
-    @p_value = "2";
+    @p_value = "2"
     if(!params || !params[:job])
-      logger.info "Inside Redirect!";
-      redirect_to :action => "index";
-      return;
+      logger.info "Inside Redirect!"
+      redirect_to :action => "index"
+      return
     end
     if(params[:job][:input_file])
-      logger.info "Reading :input_file into :input_data";
-      params[:job][:input_data] = params[:job][:input_file].read;
-      params[:job].delete(:input_file);
+      logger.info "Reading :input_file into :input_data"
+      params[:job][:input_data] = params[:job][:input_file].read
+      params[:job].delete(:input_file)
     end
-    @job = Job.new(params[:job]);
+    @job = Job.new(params[:job])
     if (@job.valid?)
-        logger.info "job.valid? " + @job.valid?.to_s;  
+        logger.info "job.valid? " + @job.valid?.to_s  
         # create the dummy file to avoid RouteErrors
-        self.write_done_file("0", "");
+        self.write_done_file("0", "")
     else 
-        logger.info "job.valid? " + @job.valid?.to_s;  
-        self.write_done_file("2", "Please check the data you input.");
-        return;
+        logger.info "job.valid? " + @job.valid?.to_s  
+        self.write_done_file("2", "Please check the data you input.")
+        return
     end
 
     params[:job].each { | key, value |
-      ENV['POLYNOME_' + key.upcase] = value;
+      ENV['POLYNOME_' + key.upcase] = value
     }
     # create file prefix using md5 check sum as part of the filename
     ENV['POLYNOME_FILE_PREFIX'] = 'files/files-' +
-    Digest::MD5.hexdigest(params[:job][:input_data]);
-    logger.info "fileprefix: "+ ENV['POLYNOME_FILE_PREFIX'];
-    @file_prefix = ENV['POLYNOME_FILE_PREFIX'];
+    Digest::MD5.hexdigest(params[:job][:input_data])
+    logger.info "fileprefix: "+ ENV['POLYNOME_FILE_PREFIX']
+    @file_prefix = ENV['POLYNOME_FILE_PREFIX']
 
     # split is also checking the input format
-    datafiles = self.split_data_into_files(params[:job][:input_data]);
+    datafiles = self.split_data_into_files(params[:job][:input_data])
     if (!datafiles)
         # TODO make this error message nice
-        @error_message = "The data you entered is invalid.";
-        self.write_done_file("2", "<font color=red>" +  @error_message + "</font><br> "); 
-        @error_message = "";
-        return; 
+        @error_message = "The data you entered is invalid."
+        self.write_done_file("2", "<font color=red>" +  @error_message + "</font><br> ") 
+        @error_message = ""
+        return 
     end
     
     # create the dummy file to avoid RouteErrors
-    self.write_done_file("0", "");
+    self.write_done_file("0", "")
 
-    n_react_threshold = 5;
-    n_stochastic_threshold = 10;
+    n_react_threshold = 5
+    n_stochastic_threshold = 10
     
     ## All checking of any input should be done before we spawn, so the user
     #receives feedback about invalid options right away and not after some time
     # ( = everything) 
     if !@job.is_deterministic && @job.nodes > n_stochastic_threshold
-      @error_message = "A stochastic model requires no more than #{n_stochastic_threshold} nodes.  Sorry!";
-      self.write_done_file("2", "<font color=red>" +  @error_message + "</font><br> ");
-      @error_message = "";
-      return;
+      @error_message = "A stochastic model requires no more than #{n_stochastic_threshold} nodes.  Sorry!"
+      self.write_done_file("2", "<font color=red>" +  @error_message + "</font><br> ")
+      @error_message = ""
+      return
     end
     
     # check for correct input options
-    logger.info "Sequential update: " + @job.sequential.to_s;
-    stochastic_sequential_update = "0";
+    logger.info "Sequential update: " + @job.sequential.to_s
+    stochastic_sequential_update = "0"
     if (@job.sequential)
-        logger.info "Update_schedule :" +@job.update_schedule + ":";
+        logger.info "Update_schedule :" +@job.update_schedule + ":"
         if ( !@job.is_deterministic )
-            logger.info "Not deterministic";
-            @error_message = "Sequential updates can only be chosen for deterministic models. Exiting";
-            self.write_done_file("2", "<font color=red>" +  @error_message+ "</font><br> "); 
-            return;
+            logger.info "Not deterministic"
+            @error_message = "Sequential updates can only be chosen for deterministic models. Exiting"
+            self.write_done_file("2", "<font color=red>" +  @error_message+ "</font><br> ") 
+            return
         end
         if ( @job.update_schedule == "")
             logger.info "Update sequential but no schedule given, doing
-            sequential udpate with random update schedule";
-            stochastic_sequential_update = "1";
+            sequential udpate with random update schedule"
+            stochastic_sequential_update = "1"
         end
     end
    
@@ -113,20 +113,20 @@ class JobsController < ApplicationController
     spawn do
         #TODO this will change to a single new filename
       discretized_datafiles = datafiles.collect { |datafile|
-        datafile.gsub(/input/, 'discretized-input');
+        datafile.gsub(/input/, 'discretized-input')
       }
 
-      self.discretize_data(datafiles, discretized_datafiles, @p_value);
+      self.discretize_data(datafiles, discretized_datafiles, @p_value)
 
       #concatenate_discretized_files
-      first = TRUE;
+      first = TRUE
       File.open( "public/perl/" + @file_prefix + ".discretized-input.txt", 'w') {
           |f| discretized_datafiles.each{ |datafile|
               if (!first)
-                  f.write("#\n");
+                  f.write("#\n")
               end
-              f.write(File.open(datafile, 'r').read);
-              first = FALSE;}
+              f.write(File.open(datafile, 'r').read)
+              first = FALSE}
       }
 
       # react: n <= n_threshold, is_deterministic
@@ -135,75 +135,75 @@ class JobsController < ApplicationController
       # error: all other cases, i.e. n > n_threshold, !is_deterministic, !data_consistent
 
       if @job.state_space
-        @job.show_functions = true;
-        @functionfile_name = self.functionfile_name(@file_prefix);
+        @job.show_functions = true
+        @functionfile_name = self.functionfile_name(@file_prefix)
       end
       
       if !@job.show_functions && !@job.wiring_diagram
-        self.write_done_file("1", "Thats all folks!");
-        return;
+        self.write_done_file("1", "Thats all folks!")
+        return
       end
       
-      do_wiring_diagram_version = @job.wiring_diagram && !@job.show_functions;
+      do_wiring_diagram_version = @job.wiring_diagram && !@job.show_functions
       # if function file is not needed, then run shorter version of minset/sgfan
       #    which produces a wiring diagram but not a function file.
       
       if @job.is_deterministic
         if @job.nodes <= n_react_threshold
           # do react
-          run_react(@job.nodes, @file_prefix, discretized_datafiles);
+          run_react(@job.nodes, @file_prefix, discretized_datafiles)
         else
           # do: makeconsistent, minsets
-          self.make_data_consistent(discretized_datafiles, @p_value, @job.nodes);
+          self.make_data_consistent(discretized_datafiles, @p_value, @job.nodes)
           if do_wiring_diagram_version
             self.minsets_generate_wiring_diagram(discretized_datafiles,
-                @job.wiring_diagram_format, @p_value, @job.nodes);
+                @job.wiring_diagram_format, @p_value, @job.nodes)
           else
-            self.minsets(discretized_datafiles, @p_value, @job.nodes);           
+            self.minsets(discretized_datafiles, @p_value, @job.nodes)           
           end
         end
       else 
-        self.make_data_consistent(discretized_datafiles, @p_value, @job.nodes);
+        self.make_data_consistent(discretized_datafiles, @p_value, @job.nodes)
         if @job.nodes <= n_stochastic_threshold
           # do sgfan
           if do_wiring_diagram_version
             self.generate_wiring_diagram(discretized_datafiles,
-                @job.wiring_diagram_format, @p_value, @job.nodes);
+                @job.wiring_diagram_format, @p_value, @job.nodes)
           else
-            self.sgfan(discretized_datafiles, @p_value, @job.nodes);
+            self.sgfan(discretized_datafiles, @p_value, @job.nodes)
           end
         else
           logger.warn("internal error: should not be here")
         end
       end
       
-      self.write_done_file("1", ""); 
+      self.write_done_file("1", "") 
       
       if !do_wiring_diagram_version
           # run simulation
-          logger.info "Starting simulation of state space.";
+          logger.info "Starting simulation of state space."
             
           if ( @job.sequential && !@job.update_schedule )
               logger.info "Update sequential but no schedule given, doing
-              sequential udpate with random update schedule";
-              stochastic_sequential_update = "1";
+              sequential udpate with random update schedule"
+              stochastic_sequential_update = "1"
           end
           
-          show_probabilities_state_space = @job.show_probabilities_state_space ?  "1" : "0";
-          wiring_diagram = @job.wiring_diagram ? "1" : "0";
+          show_probabilities_state_space = @job.show_probabilities_state_space ?  "1" : "0"
+          wiring_diagram = @job.wiring_diagram ? "1" : "0"
           logger.info "Wiring diagram: :" + wiring_diagram + ":"
-          sequential = @job.sequential ? "1" : "0";
+          sequential = @job.sequential ? "1" : "0"
 
           if ( !@job.update_schedule || @job.update_schedule == "") 
-            @job.update_schedule = "0";
+            @job.update_schedule = "0"
           else 
             # concatenate update schedule into one string with _ as separators
             # so we can pass it to dvd_stochastic_runner.pl
-            @job.update_schedule = @job.update_schedule.gsub(/\s+/, "_" );
+            @job.update_schedule = @job.update_schedule.gsub(/\s+/, "_" )
           end
-          logger.info "Update Schedule: " + @job.update_schedule;
-        @functionfile_name = self.functionfile_name(@file_prefix);
-          logger.info "Functionfile : " + @functionfile_name;
+          logger.info "Update Schedule: " + @job.update_schedule
+        @functionfile_name = self.functionfile_name(@file_prefix)
+          logger.info "Functionfile : " + @functionfile_name
 
             logger.info "perl public/perl/dvd_stochastic_runner.pl -v
             #{@job.nodes} #{@p_value.to_s} 1 #{stochastic_sequential_update}
@@ -211,20 +211,20 @@ class JobsController < ApplicationController
             #{@job.wiring_diagram_format} #{wiring_diagram} #{sequential}
             #{@job.update_schedule} #{show_probabilities_state_space} 1 0
             #{@functionfile_name}"
-          simulation_output = `perl public/perl/dvd_stochastic_runner.pl #{@job.nodes} #{@p_value.to_s} 1 #{stochastic_sequential_update} public/perl/#{@file_prefix} #{@job.state_space_format} #{@job.wiring_diagram_format} #{wiring_diagram} #{sequential} #{@job.update_schedule} #{show_probabilities_state_space} 1 0 #{@functionfile_name}`;
+          simulation_output = `perl public/perl/dvd_stochastic_runner.pl #{@job.nodes} #{@p_value.to_s} 1 #{stochastic_sequential_update} public/perl/#{@file_prefix} #{@job.state_space_format} #{@job.wiring_diagram_format} #{wiring_diagram} #{sequential} #{@job.update_schedule} #{show_probabilities_state_space} 1 0 #{@functionfile_name}`
           logger.info "simulation output: " + simulation_output
-          simulation_output = simulation_output.gsub("\n", "");
+          simulation_output = simulation_output.gsub("\n", "")
 
       end
       
-      self.write_done_file("1",  simulation_output);
+      self.write_done_file("1",  simulation_output)
     end
   end
  
   def write_done_file(done, simulation_output)
     # Tell the website we are done
-    `echo 'var done = #{done};' > public/perl/#{@file_prefix}.done.js`;
-    `echo "var simulation_output = '#{simulation_output}';" >> public/perl/#{@file_prefix}.done.js`;
+    `echo 'var done = #{done}' > public/perl/#{@file_prefix}.done.js`;
+    `echo "var simulation_output = '#{simulation_output}'" >> public/perl/#{@file_prefix}.done.js`;
   end
   
   # TODO FBH: This function is doing the checking at the moment, should
@@ -233,49 +233,49 @@ class JobsController < ApplicationController
   # code to accept single file with #
   # This should only do the error checking! 
   def split_data_into_files(data)
-    datafile = "public/perl/" + @file_prefix + ".input.txt";
+    datafile = "public/perl/" + @file_prefix + ".input.txt"
 
     File.open(datafile, 'w') {|file| file.write(data) }
 
-    datafiles = [];
-    output = NIL;
+    datafiles = []
+    output = NIL
     File.open(datafile) do |file| 
-        counter = 0;
-        something_was_written = FALSE;
+        counter = 0
+        something_was_written = FALSE
         while line = file.gets 
             # parse lines and break into different files at #
             if( line.match( /^\s*\#+/ ) )
                 if (something_was_written && output) 
-                    output.close;
-                    output = NIL;
+                    output.close
+                    output = NIL
                 end
-                something_was_written = FALSE;
+                something_was_written = FALSE
             else 
                 if (!something_was_written) 
                     outputfile_name = datafile.gsub(/input/,"input" +
-                    counter.to_s);
-                    counter +=1;
-                    output = File.open(outputfile_name, "w"); 
-                    datafiles.push(Dir.getwd + "/" + outputfile_name);
+                    counter.to_s)
+                    counter +=1
+                    output = File.open(outputfile_name, "w") 
+                    datafiles.push(Dir.getwd + "/" + outputfile_name)
                 end
                 # check if line matches @n_nodes digits
-                nodes_minus_one = (@job.nodes - 1).to_s;
+                nodes_minus_one = (@job.nodes - 1).to_s
                 if (line.match( /^\s*(\.?\d+\.?\d*\s+){#{nodes_minus_one}}\.?\d+\.?\d*\s*$/ ) ) 
-                    output.puts line;
-                    logger.info "write line" + line;
-                    something_was_written = TRUE;
+                    output.puts line
+                    logger.info "write line" + line
+                    something_was_written = TRUE
                 else
-                    logger.warn "Error: Input data not correct";
-                    return NIL;
+                    logger.warn "Error: Input data not correct"
+                    return NIL
                 end
             end
         end 
-        file.close;
+        file.close
         if (output) 
-            output.close;
+            output.close
         end
     end
-    return datafiles;
+    return datafiles
   end
  
 
@@ -289,31 +289,31 @@ class JobsController < ApplicationController
       :m2_command => "discretize(#{m2_string(infiles)}, #{m2_string(outfiles)}, #{p_value})",
       :m2_file => "Discretize.m2",
       :m2_wait => 1
-      );
+      )
   end
   
   def generate_wiring_diagram(discretized_data_files, file_format, p_value, n_nodes)
-    dotfile = self.dotfile_name(@file_prefix);
-    graphfile = self.graphfile_name(@file_prefix, file_format);
+    dotfile = self.dotfile_name(@file_prefix)
+    graphfile = self.graphfile_name(@file_prefix, file_format)
 
     logger.info macaulay2(
       :m2_command => "wd(#{m2_string(discretized_data_files)}, ///../#{dotfile}///, #{p_value}, #{n_nodes})",
       :m2_file => "wd.m2",
       :post_m2_command => "dot -T #{file_format} -o #{graphfile} #{dotfile}",
       :m2_wait => 1
-      );
+      )
   end
 
   def minsets_generate_wiring_diagram(discretized_data_files, file_format, p_value, n_nodes)
-    dotfile = self.dotfile_name(@file_prefix);
-    graphfile = self.graphfile_name(@file_prefix, file_format);
+    dotfile = self.dotfile_name(@file_prefix)
+    graphfile = self.graphfile_name(@file_prefix, file_format)
 
     macaulay2(
       :m2_command => "minsetsWD(#{m2_string(discretized_data_files)}, ///../#{dotfile}///, #{p_value}, #{n_nodes})",
       :m2_file => "minsets-web.m2",
       :post_m2_command => "dot -T #{file_format} -o #{graphfile} #{dotfile}",
       :m2_wait => 1
-      );
+      )
   end
   
   def data_consistent?(discretized_data_files, p_value, n_nodes)
@@ -321,35 +321,35 @@ class JobsController < ApplicationController
       :m2_command => "isConsistent(#{m2_string(discretized_data_files)}, #{p_value}, #{n_nodes})",
       :m2_file => "isConsistent.m2",
       :m2_wait => 1
-      );
+      )
     # 0 inconsistent
     # 1 consistent
     logger.info "data is consistent returned " + ret_val + "0 inconsistent,
-    1 consistent";
-    return ( ret_val != "0" );
+    1 consistent"
+    return ( ret_val != "0" )
   end
 
   def make_data_consistent(discretized_data_files, p_value, n_nodes)
-    logger.warn("make_data_consistent is not yet written: always just continues...");
+    logger.warn("make_data_consistent is not yet written: always just continues...")
   end
 
   def sgfan(discretized_data_files, p_value, n_nodes)
-    functionfile = self.functionfile_name(@file_prefix);
+    functionfile = self.functionfile_name(@file_prefix)
     macaulay2(
       :m2_command => "sgfan(#{m2_string(discretized_data_files)}, ///../#{functionfile}///, #{p_value}, #{n_nodes})",
       :m2_file => "func.m2",
       :m2_wait => 1
-      );
-    functionfile;
+      )
+    functionfile
   end
   
   def minsets(discretized_data_files, p_value, n_nodes)
-    functionfile = self.functionfile_name(@file_prefix);
+    functionfile = self.functionfile_name(@file_prefix)
     macaulay2(
       :m2_command => "minsets(#{m2_string(discretized_data_files)}, ///../#{functionfile}///, #{p_value}, #{n_nodes})",
       :m2_file => "minsets-web.m2",
       :m2_wait => 1
-      );
-    functionfile;
+      )
+    functionfile
   end
 end
