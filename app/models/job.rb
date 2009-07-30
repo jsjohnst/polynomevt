96d0a5c18156390063a18b65dec6168e644df86c
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-07-29 22:30:00 -0400

    validate update schedule in models/jobs.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,12 @@
 class Job < ActiveRecord::Base
-  validates_presence_of :nodes
-  validates_numericality_of :nodes, :only_integer => true, :message => "Number of nodes must be an integer between 1 and 11"
-  validates_numericality_of :nodes, :less_than => 12, :message => "Number of nodes is too big!"
-  validates_numericality_of :nodes, :greater_than => 0, :message => "Number of nodes is too small!"
+  validates_presence_of :nodes, :on => :create
+  validates_numericality_of :nodes, :only_integer => true, :message => "Number of nodes must be an integer between 1 and 11", :on => :create
+  validates_numericality_of :nodes, :less_than => 12, :message => "Number of nodes is too big!", :on => :create
+  validates_numericality_of :nodes, :greater_than => 0, :message => "Number of nodes is too small!", :on => :create
 
+  # check update schedule to be blank or the right reges
+  validates_format_of :update_schedule, :with => /^\s*(\d+\s*){#{:nodes}}\d+\s*$/, :allow_blank => :true, :on => :create
+  
   def file_prefix
     @file_prefix
   end
