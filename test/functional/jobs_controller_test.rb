bd6680dccb43a0642e22905324d632deb712fe67
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-07-31 02:35:17 -0400

    test to check if input.txt is being generated for one fixture, using done.js to decide how long to wait until checking for file

--- test/functional/jobs_controller_test.rb
+++ test/functional/jobs_controller_test.rb
@@ -18,25 +18,28 @@ class JobsControllerTest < ActionController::TestCase
   test "should upload input.txt" do
     job = jobs(:one)
     result = JobsController.new.generate_output_of(job)
-    sleep 4
     prefix = "public/perl/" + job.file_prefix
-    input_data = prefix + ".input.txt"
-    assert  FileTest.exists?("#{input_data}"), "#{input_data} does not exist"
     done_file = prefix + ".done.js"
     line = ""
-    File.open(done_file, 'r')  do |file| 
-        line = file.gets 
+    waiting = true
+    while (waiting)
+        File.open(done_file, 'r')  do |file| 
+            line = file.gets 
+        end
+
+        assert line.match( /^var\sdone\s=\s/ ), message = "1st line in done.js did not match var"
+        if line.match( /^var\sdone\s=\s1/ )
+            #print "line did not match var done = 1"
+            waiting = false
+        else
+            sleep 1 
+        end
     end
-    unless line.match( /^var\sdone\s=\s/ )
-        print "line did not match var"
-    end
-    #unless line.match( /^var\sdone\s=\s0/ )
-    #    print "line did not match var done = 0"
-    #end
-    unless line.match( /^var\sdone\s=\s1/ )
-        print "line did not match var done = 1"
-    end
-    #`rm public/perl/#{job.file_prefix}*`
+    input_data = prefix + ".input.txt"
+    assert  FileTest.exists?("#{input_data}"), "#{input_data} does not exist"
+    assert  !FileTest.exists?("#{input_data}.dummy"), "#{input_data}.dummy does not exist"
+    `rm #{prefix}*`
+    print "rm #{prefix}*\n"
   end
 
   test "generate 1" do
@@ -44,7 +47,7 @@ class JobsControllerTest < ActionController::TestCase
     result = JobsController.new.generate_output_of(job)
 
     print "about to sleep"
-    sleep 4
+    sleep 1 
     print "done sleeping"
 
     input_data = "public/perl/" + job.file_prefix + ".input.txt"
