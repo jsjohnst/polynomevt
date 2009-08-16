2e4a224746894612006bae094ee3b6bf5e053359
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-08-16 12:11:52 -0400

    alloweing big networks

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
   validates_presence_of :nodes, :on => :create
   validates_numericality_of :nodes, :only_integer => true, :message => "Number of nodes must be an integer between 1 and 11", :on => :create
-  validates_numericality_of :nodes, :less_than => 50, :message => "Number of nodes is too big!", :on => :create
+  validates_numericality_of :nodes, :less_than => 150, :message => "Number of nodes is too big!", :on => :create
   validates_numericality_of :nodes, :greater_than => 0, :message => "Number of nodes is too small!", :on => :create
   validate :check_update_schedule
 
