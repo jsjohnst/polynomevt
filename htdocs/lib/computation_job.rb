4f7b989ec302b0550af59415431273b45aa6f844
Jeremy Johnstone <github@jeremyjohnstone.com>
2009-08-24 21:23:48 -0700

    stubbing out the basics of a ComputationJob which is run by delayed_job to do the meat and potatoes of the actual work

--- htdocs/lib/computation_job.rb
+++ htdocs/lib/computation_job.rb
@@ -1,6 +1,36 @@
+require 'digest/md5'
+
 class ComputationJob < Struct.new(:job_id)  
   def perform  
     # here is where we will handle the background Macaulay processing  
     # and simulation of the network
+    
+    # first we go fetch the job so we know what we need to do
+    @job = Job.find(job_id)
+    
+    # now we setup a unique path to this job's data
+    @job.file_prefix = 'files/files-' + Digest::MD5.hexdigest( @job.id.to_s )
+    
+    # split is also checking the input format
+    datafile = "public/" + @job.file_prefix + ".input.txt"
+    File.open(datafile, 'w') {|file| file.write(@job.input_data) }
+    
+    # we succeeded if we got to here!
+    self.success()
   end  
+  
+  def abort
+    @job.failed = true
+    self.completed
+  end
+  
+  def success
+    @job.failed = false
+    self.completed
+  end
+  
+  def completed
+    @job.completed = true
+    @job.save
+  end
 end
