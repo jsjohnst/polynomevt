86d3fca7c8a818a0cc28de09149cade0ec4c10f0
Jeremy Johnstone <github@jeremyjohnstone.com>
2009-08-24 19:56:39 -0700

    found the issue! we were updating the nodes to be 5 and the update_schedule was still set to '1 2 3' which obviously doesn't have five nodes.

--- htdocs/test/functional/jobs_controller_test.rb
+++ htdocs/test/functional/jobs_controller_test.rb
@@ -31,7 +31,7 @@ class JobsControllerTest < ActionController::TestCase
   end
 
   test "should update job" do
-    put :update, :id => jobs(:one).to_param, :job => { :user_id => 1, :nodes => 5, :pvalue => 2 }
+    put :update, :id => jobs(:one).to_param, :job => { :user_id => 1, :nodes => 5, :pvalue => 2, :update_schedule => "1 2 3 4 5" }
     assert_redirected_to job_path(assigns(:job))
   end
 
