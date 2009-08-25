e8048ee369693553316ded6ac51414e1c53201d8
Jeremy Johnstone <github@jeremyjohnstone.com>
2009-08-25 06:41:53 -0700

    Copied over stochastic model check from the v1 jobs controller and modified it to work inside the model (where it really should be in the v2 world) instead of being in the controller.

--- htdocs/app/models/job.rb
+++ htdocs/app/models/job.rb
@@ -7,11 +7,19 @@ class Job < ActiveRecord::Base
   validates_numericality_of :nodes, :greater_than => 0, :message => "Number of nodes is too small!"
   validates_numericality_of :pvalue, :equal_to => 2
   validate :check_update_schedule
+  validate :check_stochastic_state_space_size
   
   def check_update_schedule
     if update_schedule
       errors.add_to_base("Update schedule not valid " + update_schedule) unless
       update_schedule.match( /^\s*((\d+\s*){#{nodes}})?\s*$/ )
+    end
+  end
+  
+  def check_stochastic_state_space_size
+    if make_deterministic_model && show_probabilities_state_space
+      errors.add_to_base("A stochastic model with more than 10 nodes cannot be simulated.  Sorry!") unless
+      nodes <= 10 # if you update this, be sure and update the error message above too
     end
   end
 end
