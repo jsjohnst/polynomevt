f39481bd8e8647eab8fbe3f2e83e8a199cd9ca41
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-08-25 22:51:52 -0400

    moving tests for user from functional to unit tests where they should really go

--- htdocs/test/functional/users_controller_test.rb
+++ htdocs/test/functional/users_controller_test.rb
@@ -12,52 +12,6 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should not save invalid user" do 
-    my_user = users(:invalid_user_without_login)
-    assert !my_user.save, "should not save user without login"
-    my_user = users(:invalid_user_with_invalid_email)
-    assert !my_user.save, "should not save user with invalid email"
-  end 
-
-  test "should save simple user" do 
-    my_user = users(:valid_user)
-    assert my_user.valid?
-    assert my_user.save, "should save this user, or not?"
-  end
-
-  test "should not save user with too short login" do 
-    my_user = users(:invalid_login)
-    assert !my_user.valid?
-  end
-
-  test "should not allow duplicate email addresses" do 
-    my_user1 = User.new({ :login => "simpleuserfirst",
-      :password => "simpleuserspassword",
-      :email => "doubletwo@address.com",
-      :first_name => "Double",
-      :last_name => "User"})
-    assert my_user1.save, "first user should have been saved" 
-    my_user2 = User.new({ :login => "simpleusersecond",
-      :password => "simpleuserspassword",
-      :email => "doubletwo@address.com",
-      :first_name => "Double",
-      :last_name => "User"})
-    assert !my_user2.save, "second user with same email address should not have been saved"
-  end
-  
-  test "should allow two users with empty email address" do
-    my_user1 = User.new({ :login => "firstwithoutemail",
-      :password => "simpleuserspassword",
-      :first_name => "First",
-      :last_name => "User"})
-    assert my_user1.save, "first user without email address should have been saved" 
-    my_user2 = User.new({ :login => "secondwithoutemail",
-      :password => "simpleuserspassword",
-      :first_name => "Second",
-      :last_name => "User"})
-    assert my_user2.save, "second user without email address should have been saved"
-  end
-
   test "should create user" do
     assert_difference('User.count') do
       post :create, :user => { :login => "useronlywithpassword", :password => "fubarbaz" }
