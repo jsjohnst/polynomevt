a2757ea1e7adfd77d68908e6a581be4a9921363d
Jeremy Johnstone <jstone@air-33j-net.local>
2009-09-07 11:18:09 -0700

    Adding a unit test for email delivery of credentials.

--- htdocs/test/unit/emailer_test.rb
+++ htdocs/test/unit/emailer_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class EmailerTest < ActionMailer::TestCase
-  # replace this with your real tests
-  test "the truth" do
-    assert true
+  tests Emailer
+  
+  def setup
+    ActionMailer::Base.delivery_method = :test
+  end
+  
+  test "test credential delivery" do
+    email = Emailer.deliver_credentials("fhinkel@vt.edu", "test_login", "test_password")
+    assert !ActionMailer::Base.deliveries.empty?
+
+    # Test the body of the sent email contains what we expect it to
+    assert_equal ["fhinkel@vt.edu"], email.to
+    assert_equal "Polynome - Lost credentials information", email.subject
+    assert_match /Login: test_login/, email.body
+    assert_match /Password: test_password/, email.body
   end
 end
