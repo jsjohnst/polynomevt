9ea4be82e926c36c41a852a02be1dc00cfe6f4c2
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-09-08 07:13:35 -0400

    change dvd_stochastic_runner to use Getopt::Long parameters. Removed export from DVDCore.

--- perl/dvd_stochastic_runner.pl
+++ perl/dvd_stochastic_runner.pl
@@ -6,9 +6,6 @@
 ## Functions can be read from the input file with the possiblity for more than one function per node
 ## Authored by Jonte Craighead and Franziska Hinkelmann
 #
-# If this script is run, Stochastic is hard coded to 1, so we always get a
-# probabilities in the graph (maybe this should be changed?)
-#
 # The all_trajectories_flag set means all possible arrows are drawn in one
 # graph, without this option, one update is choosen at random for every
 # variables - the program should always be run with the flag turned on, since
@@ -28,73 +25,104 @@ use strict;
 
 use lib '../perl';
 
-use DVDCore qw($Clientip $Function_data $Function_file &error_check @Output_array $Pwd &dvd_session &_log $Stochastic);
+use DVDCore;# qw($Clientip $Function_data $Function_file &error_check @Output_array $Pwd &dvd_session &_log $show_probabilities_in_state_space);
 use Cwd;
-use Getopt::Std;
-getopts('vhd');
- 
-our ($opt_d, $opt_v, $opt_h);
-my $DEBUG=$opt_d;
-my $MINDEBUG=$opt_v;
 
-if ($DEBUG) {
-    $MINDEBUG = 1;
+#use Getopt::Std;
+
+my ($debug, $verbose, $help);
+my $n_nodes;
+my $p_value = 2;
+my $all_trajectories_flag = 1;
+  # This flag set means all possible arrows are drawn in one
+  # graph, without this option, one update is choosen at random for every
+  # variables
+
+my $update_stochastic_flag = 0;
+ 	# if set, an update stochastic system is simulated using
+  # random delays
+
+my $file_prefix; #outputfiles
+my $statespace_format = "gif"; # state_space format
+my $wiring_diagram_format =  "gif"; # wiring_diagram format
+
+my $show_wiring_diagram = 0 ; # on if wiring diagram should be graphed
+my $show_statespace = 0; # show_statespace 1 means create picture
+
+my $update_sequential_flag = 0;
+  # 1 for sequential update, 0 for synchronous updates
+
+my $update_schedule = 0; # update_schedule
+  # has to be set to 0 for random sequential updates (i.e., update_stochastic_flag == 1 )
+
+my $show_probabilities_in_state_space = 0; 	# if set to one, probabilities are included in graph of state space 
+
+my $show_trajectory_from_one_state = 0; # 0 if all transitions, 1 for transitions from one initial state
+my $trajectory_initial_state = 0; # initial state if printing just one trajectory
+
+my $input_function_file; 
+
+use Getopt::Long;
+
+
+GetOptions ( "v" => \$verbose,    # flag
+             "h" => \$help,       # flag
+             "d" => \$debug,      # flag
+             "nodes=i" => \$n_nodes, # numerical
+             "pvalue=i" => \$p_value, # numerical
+             "all_trajectories" => \$all_trajectories_flag,   # flag
+             "update_stochastic" => \$update_stochastic_flag,  # flag
+             "file_prefix=s" => \$file_prefix,  # string
+             "statespace_format=s" => \$statespace_format, # string
+             "wiring_diagram_format=s" => \$wiring_diagram_format, # string
+             "show_wiring_diagram" => \$show_wiring_diagram, #flag
+             "show_statespace" => \$show_statespace, #flag
+             "update_sequential" => \$update_stochastic_flag, 
+             "update_schedule=s" => \$update_schedule, #string
+             "show_probabilities_in_state_space" => \$show_probabilities_in_state_space, 
+             "show_trajectory_from_one_state" => \$show_trajectory_from_one_state,
+             "trajectory_initial_state=s" => \$trajectory_initial_state,
+             "function_file=s" => \$input_function_file #string
+             ) ; 
+
+if ($debug) {
+    $verbose = 1;
 }
 
-if ($DEBUG) {print getcwd;}
-if ($MINDEBUG) {print "Using Minimum Debug output for dvd_stochastic_runner.pl<br>\n";}
-if ($DEBUG) { print "<br>Number of arguments (should equal 12) " . $#ARGV .  "<br>"; }
+if ($debug) {print getcwd . "<br>\n";}
+if ($verbose) {print "Using Minimum Debug output for dvd_stochastic_runner.pl<br>\n";}
 
-die "Usage: dvd_stochasitic_runner.pl [-vdh] #nodes #states all_trajectories_flag update_stochastic_flag outputfilename statespace_format wiring_diagram_format show_wiring_diagram show_statespace update_sequential_flag update_schedule Probabilities_in_graph_flag trajectory_flag trajectory_value inputfile.txt \n\t-v minimal debug output \n\t-d debug output \n\t-h  this help\n" if ($opt_h || $#ARGV != 14);
+die "Usage: Read the source code!
+\n" if ($help ||
+!$input_function_file || !$n_nodes || !$file_prefix );
 
-my $n_nodes = $ARGV[0]; #number of variables
-my $p_value = $ARGV[1]; #number of states
-my $all_trajectories_flag= $ARGV[2];  # This flag set means all possible arrows are drawn in one
-    # graph, without this option, one update is choosen at random for every
-    # variables
-my $update_stochastic_flag=$ARGV[3]; 	#if set, an update stochastic system is simulated using
-    #random delays
-my $file_prefix = $ARGV[4]; #outputfiles
-my $statespace_format = $ARGV[5]; #graph format
-my $wiring_diagram_format = $ARGV[6];
-my $show_wiring_diagram = $ARGV[7]; #on if wiring diagram should be graphed
-my $show_statespace = $ARGV[8]; #show_statespace 1 means create picture
-my $update_sequential_flag = $ARGV[9]; #1 if sequential update, has to be set to 0 for random sequential updates (i.e., update_stochastic_flag == 1 )
-my $update_schedule = $ARGV[10]; #update_schedule
-my $Stochastic = $ARGV[11]; 	# if set to one, probabilities are included in graph of state space 
-my $trajectory_flag = $ARGV[12]; # 1 if all trajectories, 0 for a single trajetory form intitial state trajectory_value
-my $trajectory_value = $ARGV[13]; # initial state
-my $stochastic_input_file = $ARGV[-1]; 
 
-if ($DEBUG) {print "All trajectories flag, 1 if all trajectories, 0 for a single initial state: $trajectory_flag \n <br>"; }
-
-if ($MINDEBUG) {print "Number of nodes $n_nodes <br>
+if ($verbose) {print "    Debug :$debug: <br>
+    Number of nodes $n_nodes <br>
     P_value :$p_value: <br>
     file_prefix :$file_prefix: <br>
     Statespace format :$statespace_format: <br>
     Wiring diagram format :$wiring_diagram_format:<br>
     Show Wiring diagram :$show_wiring_diagram:<br>
     Show Statespace :$show_statespace: <br>
-    Functionfile :$stochastic_input_file: <br>
+    Functionfile :$input_function_file: <br>
     Update sequential :$update_sequential_flag: <br>
     Update schedule :$update_schedule: <br>
     Update stochastic :$update_stochastic_flag: <br> 
-    show_probabilities_state_space :$Stochastic: <br>";}
+    Show_probabilities_state_space :$show_probabilities_in_state_space: <br>\n";}
 
-open(my $function_file, $stochastic_input_file);
-_log("Attempted to read from '$stochastic_input_file'");
+
+open (my $function_file, $input_function_file) or die $!;
+print ("Attempted to read from '$input_function_file'\n");
 
 my $Pwd = getcwd();
 
-my @response = dvd_session($n_nodes, $p_value, $file_prefix, 0,
-$update_sequential_flag, $update_schedule, $all_trajectories_flag,
-$show_statespace, $statespace_format, $show_wiring_diagram, $wiring_diagram_format,
-$trajectory_flag, $trajectory_value, $update_stochastic_flag, $Stochastic, $DEBUG,
-$function_file);
+my @response = DVDCore::dvd_session($n_nodes, $p_value, $file_prefix, 0, $update_sequential_flag, $update_schedule, $all_trajectories_flag, $show_statespace, $statespace_format, $show_wiring_diagram, $wiring_diagram_format, $show_trajectory_from_one_state, $trajectory_initial_state, $update_stochastic_flag, $show_probabilities_in_state_space, $debug, $function_file);
 
 if($response[0] == 1) { # a response code should always be returned by the main DVDCore functions
+    my @Output_array = shift(@response);
     _log($_) foreach(@Output_array);
-    if ($trajectory_flag) {
+    if ($show_trajectory_from_one_state) {
         print "Number of components $Output_array[2]<br>";
         print "Number of fixed points $Output_array[3]<br>";
         print "$Output_array[5]<br>";
