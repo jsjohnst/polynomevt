42500b6cd7637f61d714e3d27dd0d1c196fb1ffb
Jeremy Johnstone <jstone@air-33j-net.local>
2009-09-18 00:29:07 -0700

    Added auto-closing of panels on page load which should start closed.

--- htdocs/public/javascripts/polynome.js
+++ htdocs/public/javascripts/polynome.js
@@ -2,6 +2,14 @@ function toggle_panel(element, id) {
 	Effect.toggle(id, 'blind', { 
 		afterFinish: function(effect) { 
 			element.className = effect.factor == -1 ? 'panel_header closed' : 'panel_header open'; 
-		} 
+		},
+		duration: 0.5 
 	});
 }
+
+function close_hidden_panels() {
+	closed_panels = $$('.panel_header.closed');
+	closed_panels.invoke('onclick');
+}
+
+Event.observe(window, 'load', close_hidden_panels);
