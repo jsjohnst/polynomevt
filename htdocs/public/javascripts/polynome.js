ab1dc134137fba4780b29bc89e2a02e8e12f68a4
Jeremy Johnstone <jstone@air-33j-net.local>
2009-09-18 11:22:27 -0700

    Added a new helper method to invoke dialogs

--- htdocs/public/javascripts/polynome.js
+++ htdocs/public/javascripts/polynome.js
@@ -7,6 +7,15 @@ function toggle_panel(element, id) {
 	});
 }
 
+function show_modal(id, title) {
+	Modalbox.show( $(id), {title: title, width: 300, overlayDuration: 0.1, slideDownDuration: 0.1,
+			slideUpDuration: 0.1, autoFocusing: false, resizeDuration: 0.1, afterLoad: function() {
+		$('dialog_' + id).value = $('hidden_' + id).value;
+	}, beforeHide: function() {
+		$('hidden_' + id).value = $('dialog_' + id).value;
+	}});
+}
+
 function close_hidden_panels() {
 	closed_panels = $$('.panel_header.closed');
 	closed_panels.invoke('onclick');
