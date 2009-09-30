8d8d397bbd9793c88dab97608779a336e8a08a65
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-09-29 21:24:00 -0400

    only add networks with perturbations affection correct number of genes

--- dream4/React100/findOverlappingWiringDiagrams.rb
+++ dream4/React100/findOverlappingWiringDiagrams.rb
@@ -1,6 +1,27 @@
 irb
 require "lib/dvdcore.rb"
 
+# for now this is hard coded for 10 node networks
+def thirty_percent_perturbation(network)
+  all_genes_affected_by_perts = 0
+  for j in 10..14 do  
+    # for each perturbation sum up in how many networks it appears
+    genes_affected_by_pert_j = 0
+    for i in 0..9 do  
+      genes_affected_by_pert_j += network[i][j]
+      all_genes_affected_by_perts += network[i][j]
+    end
+    # each perturbation must affect 2-4 genes
+    unless (2 <= genes_affected_by_pert_j && genes_affected_by_pert_j <=4)
+      return false
+    end
+    # 5 perturbations affecting ~1/3 of the genes => we expect 50/3 = 16.6
+    in_range = 13<all_genes_affected_by_perts && all_genes_affected_by_perts<19
+    return  
+  end
+end
+
+
 def add_componentwise(sum, output) 
   for i in 0..output.length-1 do 
     for j in 0..output[i].length-1 do
@@ -68,7 +89,12 @@ for k in 1..5 do
     dvd = DVDCore.new(file_prefix, 110, 2)
     dvd.run
     output = dvd.observe_dependencies_in_array
-    add_componentwise(sum, output)
+     #  Only count the networks with the perturbations affecting the correct
+     #  number of genes
+    if thirty_percent_perturbations(output) 
+      add_componentwise(sum, output)
+    end
+
   end
   outfile = File.new("/Users/fhinkel/Sites/github/htdocs/public/files/output-#{k}-edges.txt", "w")
     for i in 0..sum.length-1 do 
