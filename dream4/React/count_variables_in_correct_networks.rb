1d31c9e1ec724ca1fbb74b0ba597d05a6d4eb087
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-09-30 00:24:15 -0400

    fixed mistakes in indexing

--- dream4/React/count_variables_in_correct_networks.rb
+++ dream4/React/count_variables_in_correct_networks.rb
@@ -1,26 +1,31 @@
 require "dvdcore.rb"
 
 # for now this is hard coded for 10 node networks
-def thirty_percent_perturbation(network)
+def thirty_percent_perturbations(network)
   all_genes_affected_by_perts = 0
   first_pert = network[0].length - 5
-  last_pert = network[0].length 
+  last_pert = network[0].length - 1
   last_gene = first_pert - 1
-  for j in (first_pert-1)..(last_pert-1) do  
+  for j in (first_pert)..(last_pert) do  
     # for each perturbation sum up in how many networks it appears
     genes_affected_by_pert_j = 0
-    for i in 0..(last_pert-1) do  
+    for i in 0..last_gene do  
       genes_affected_by_pert_j += network[i][j]
       all_genes_affected_by_perts += network[i][j]
     end
+    puts "Genes affected by perturbation #{j}: #{genes_affected_by_pert_j}"
     # each perturbation must affect 2-4 genes
-    unless (2 <= genes_affected_by_pert_j && genes_affected_by_pert_j <=4)
+    unless (2 <= genes_affected_by_pert_j && genes_affected_by_pert_j <=5)
+    #unless (2 <= genes_affected_by_pert_j && genes_affected_by_pert_j <=4)
       return false
     end
-    # 5 perturbations affecting ~1/3 of the genes => we expect 50/3 = 16.6
-    in_range = 13<all_genes_affected_by_perts && all_genes_affected_by_perts<19
-    return  
   end
+  # 5 perturbations affecting ~1/3 of the genes => we expect 50/3 = 16.6
+  in_range = 13<all_genes_affected_by_perts && all_genes_affected_by_perts<19
+  if (in_range) 
+    puts "Range is ok"
+  end
+  return in_range
 end
 
 
@@ -78,9 +83,10 @@ def split_data_into_files(datafile)
 end
 
 
-require "lib/dvdcore.rb"
-
 for k in 1..5 do 
+  # networks that we don't discard because the perturbations affect too many
+  # or little genes
+  number_of_good_networks = 0
   sum = Array.new()
   output = Array.new()
   functionfiles = split_data_into_files("output-#{k}.txt")
@@ -95,13 +101,15 @@ for k in 1..5 do
      #  number of genes
     if thirty_percent_perturbations(output) 
       add_componentwise(sum, output)
+      number_of_good_networks += 1
     end
 
   end
-  outfile = File.new("/Users/fhinkel/Sites/github/htdocs/public/files/output-#{k}-edges.txt", "w")
+  outfile = File.new("output-only-good_networks-#{k}-edges.txt", "w")
     for i in 0..sum.length-1 do 
       for j in 0..sum[i].length-1 do
-        outfile.print sum[i][j] * 100 / 130
+        outfile.print sum[i][j] * 100 / number_of_good_networks 
+        #outfile.print sum[i][j] * 100 / 130
         outfile.print "\t"
       end
       outfile.puts ""
@@ -110,7 +118,5 @@ for k in 1..5 do
 end
 
 
-
-quit
 
 
