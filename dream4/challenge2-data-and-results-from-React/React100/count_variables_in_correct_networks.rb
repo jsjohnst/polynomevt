e38d5151af80f6d42777426e64496ee80b93554a
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-10-13 16:27:27 -0400

    divide by correct number of networks

--- dream4/challenge2-data-and-results-from-React/React100/count_variables_in_correct_networks.rb
+++ dream4/challenge2-data-and-results-from-React/React100/count_variables_in_correct_networks.rb
@@ -1,27 +1,6 @@
 irb
 require "lib/dvdcore.rb"
 
-# for now this is hard coded for 10 node networks
-def thirty_percent_perturbation(network)
-  all_genes_affected_by_perts = 0
-  for j in 10..14 do  
-    # for each perturbation sum up in how many networks it appears
-    genes_affected_by_pert_j = 0
-    for i in 0..9 do  
-      genes_affected_by_pert_j += network[i][j]
-      all_genes_affected_by_perts += network[i][j]
-    end
-    # each perturbation must affect 2-4 genes
-    unless (2 <= genes_affected_by_pert_j && genes_affected_by_pert_j <=4)
-      return false
-    end
-    # 5 perturbations affecting ~1/3 of the genes => we expect 50/3 = 16.6
-    in_range = 13<all_genes_affected_by_perts && all_genes_affected_by_perts<19
-    return  
-  end
-end
-
-
 def add_componentwise(sum, output) 
   for i in 0..output.length-1 do 
     for j in 0..output[i].length-1 do
@@ -81,11 +60,9 @@ require "lib/dvdcore.rb"
 for k in 1..5 do 
   sum = Array.new()
   output = Array.new()
-  functionfiles = split_data_into_files("/Users/fhinkel/Sites/github/htdocs/public/files/output-#{k}.txt")
+  functionfiles = split_data_into_files("output-#{k}.txt")
   functionfiles.each do |functionfile|
     file_prefix = functionfile.sub(/\.functionfile\.txt/, "")
-    file_prefix.sub!(/\/Users\/fhinkel\/Sites\/github\/htdocs\//,"")
-    puts file_prefix
     dvd = DVDCore.new(file_prefix, 110, 2)
     dvd.run
     output = dvd.observe_dependencies_in_array
@@ -96,10 +73,10 @@ for k in 1..5 do
     end
 
   end
-  outfile = File.new("/Users/fhinkel/Sites/github/htdocs/public/files/output-#{k}-edges.txt", "w")
+  outfile = File.new("output-#{k}-edges.txt", "w")
     for i in 0..sum.length-1 do 
       for j in 0..sum[i].length-1 do
-        outfile.print sum[i][j] * 100 / 130
+        outfile.print sum[i][j] * 100 / 15
         outfile.print "\t"
       end
       outfile.puts ""
