526994bc88710c5fd53252160a7bb25c6f5a02e8
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-10-13 22:25:50 -0400

    EOS instead of quotes for test cases

--- htdocs/lib/dvdcore_test.rb
+++ htdocs/lib/dvdcore_test.rb
@@ -69,89 +69,95 @@ node7 -> node2 [label= "1.00"];
 }
     EOS
 
-  stochastic_function_file = "f1 = {
-  x1+x2   #.9
-  x1      #.1
-  }
-  f2 = x1*x2*x3
-  f3 = {
-  x1*x2+x3^2
-  x2
-  }"
+  stochastic_function_file = <<-EOS
+f1 = {
+x1+x2   #.9
+x1      #.1
+}
+f2 = x1*x2*x3
+f3 = {
+x1*x2+x3^2
+x2
+}
+    EOS
 
-  stochastic_wiring_diagram="
-  digraph test {
-  node1 -> node1;
-  node1 -> node2;
-  node1 -> node3;
-  node1 [label="x1", shape="box"];
-  node2 -> node1;
-  node2 -> node2;
-  node2 -> node3;
-  node2 [label="x2", shape="box"];
-  node3 -> node2;
-  node3 -> node3;
-  node3 [label="x3", shape="box"];
-  }"
+  stochastic_wiring_diagram= <<-EOS
+digraph test {
+node1 -> node1;
+node1 -> node2;
+node1 -> node3;
+node1 [label="x1", shape="box"];
+node2 -> node1;
+node2 -> node2;
+node2 -> node3;
+node2 [label="x2", shape="box"];
+node3 -> node2;
+node3 -> node3;
+node3 [label="x3", shape="box"];
+}
+  EOS
 
-  stochastic_state_space = "
-  digraph test {
-  node0 [label=" 0 0 0"];
-  node1 [label=" 0 0 1"];
-  node2 [label=" 0 1 0"];
-  node3 [label=" 0 1 1"];
-  node4 [label=" 1 0 0"];
-  node5 [label=" 1 0 1"];
-  node6 [label=" 1 1 0"];
-  node7 [label=" 1 1 1"];
-  node0 -> node0;
-  node1 -> node0;
-  node1 -> node1;
-  node2 -> node0;
-  node2 -> node1;
-  node2 -> node4;
-  node2 -> node5;
-  node3 -> node1;
-  node3 -> node5;
-  node4 -> node4;
-  node5 -> node4;
-  node5 -> node5;
-  node6 -> node1;
-  node6 -> node5;
-  node7 -> node2;
-  node7 -> node3;
-  node7 -> node6;
-  node7 -> node7;
-  }"
-  stochastic_state_space_with_probabilities = "
-  digraph test {
-  node0 [label=" 0 0 0"];
-  node1 [label=" 0 0 1"];
-  node2 [label=" 0 1 0"];
-  node3 [label=" 0 1 1"];
-  node4 [label=" 1 0 0"];
-  node5 [label=" 1 0 1"];
-  node6 [label=" 1 1 0"];
-  node7 [label=" 1 1 1"];
-  node0 -> node0 [label= "1.00"];
-  node1 -> node0 [label= "0.50"];
-  node1 -> node1 [label= "0.50"];
-  node2 -> node0 [label= "0.05"];
-  node2 -> node1 [label= "0.05"];
-  node2 -> node4 [label= "0.45"];
-  node2 -> node5 [label= "0.45"];
-  node3 -> node1 [label= "0.10"];
-  node3 -> node5 [label= "0.90"];
-  node4 -> node4 [label= "1.00"];
-  node5 -> node4 [label= "0.50"];
-  node5 -> node5 [label= "0.50"];
-  node6 -> node1 [label= "0.90"];
-  node6 -> node5 [label= "0.10"];
-  node7 -> node2 [label= "0.45"];
-  node7 -> node3 [label= "0.45"];
-  node7 -> node6 [label= "0.05"];
-  node7 -> node7 [label= "0.05"];
-  }"
+  stochastic_state_space = <<-EOS
+digraph test {
+node0 [label=" 0 0 0"];
+node1 [label=" 0 0 1"];
+node2 [label=" 0 1 0"];
+node3 [label=" 0 1 1"];
+node4 [label=" 1 0 0"];
+node5 [label=" 1 0 1"];
+node6 [label=" 1 1 0"];
+node7 [label=" 1 1 1"];
+node0 -> node0;
+node1 -> node0;
+node1 -> node1;
+node2 -> node0;
+node2 -> node1;
+node2 -> node4;
+node2 -> node5;
+node3 -> node1;
+node3 -> node5;
+node4 -> node4;
+node5 -> node4;
+node5 -> node5;
+node6 -> node1;
+node6 -> node5;
+node7 -> node2;
+node7 -> node3;
+node7 -> node6;
+node7 -> node7;
+}
+  EOS
+
+stochastic_state_space_with_probabilities = <<-EOS
+digraph test {
+node0 [label=" 0 0 0"];
+node1 [label=" 0 0 1"];
+node2 [label=" 0 1 0"];
+node3 [label=" 0 1 1"];
+node4 [label=" 1 0 0"];
+node5 [label=" 1 0 1"];
+node6 [label=" 1 1 0"];
+node7 [label=" 1 1 1"];
+node0 -> node0 [label= "1.00"];
+node1 -> node0 [label= "0.50"];
+node1 -> node1 [label= "0.50"];
+node2 -> node0 [label= "0.05"];
+node2 -> node1 [label= "0.05"];
+node2 -> node4 [label= "0.45"];
+node2 -> node5 [label= "0.45"];
+node3 -> node1 [label= "0.10"];
+node3 -> node5 [label= "0.90"];
+node4 -> node4 [label= "1.00"];
+node5 -> node4 [label= "0.50"];
+node5 -> node5 [label= "0.50"];
+node6 -> node1 [label= "0.90"];
+node6 -> node5 [label= "0.10"];
+node7 -> node2 [label= "0.45"];
+node7 -> node3 [label= "0.45"];
+node7 -> node6 [label= "0.05"];
+node7 -> node7 [label= "0.05"];
+}
+  EOS
 
   test "creates wiring diagram" do
   end
