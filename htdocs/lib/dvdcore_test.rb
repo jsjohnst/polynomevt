a94a04cf6215719e3d856e4a415fe8a2d8d6dc9b
Jeremy Johnstone <jstone@air-33j-net.local>
2009-10-13 19:21:22 -0700

    Switched to using Ruby's enhanced heredoc

--- htdocs/lib/dvdcore_test.rb
+++ htdocs/lib/dvdcore_test.rb
@@ -5,65 +5,71 @@ class DvdcoreTest < ActiveSupport::TestCase
     assert true
   end
 
-  deterministic_function_file = "f1 = x1+x2
-  f2 = x1*x2*x3
-  f3 = x1*x2+x3^2"
+  deterministic_function_file = <<-EOS
+f1 = x1+x2
+f2 = x1*x2*x3
+f3 = x1*x2+x3^2
+      EOS
 
-  deterministic_wiring_diagram = "digraph test {
-  node1 -> node1;
-  node1 -> node2;
-  node1 -> node3;
-  node1 [label="x1", shape="box"];
-  node2 -> node1;
-  node2 -> node2;
-  node2 -> node3;
-  node2 [label="x2", shape="box"];
-  node3 -> node2;
-  node3 -> node3;
-  node3 [label="x3", shape="box"];
-  }"
+  deterministic_wiring_diagram = <<-EOS
+digraph test {
+node1 -> node1;
+node1 -> node2;
+node1 -> node3;
+node1 [label="x1", shape="box"];
+node2 -> node1;
+node2 -> node2;
+node2 -> node3;
+node2 [label="x2", shape="box"];
+node3 -> node2;
+node3 -> node3;
+node3 [label="x3", shape="box"];
+}
+    EOS
 
-  deterministic_state_space = "digraph test {
-  node0 [label=" 0 0 0"];
-  node1 [label=" 0 0 1"];
-  node2 [label=" 0 1 0"];
-  node3 [label=" 0 1 1"];
-  node4 [label=" 1 0 0"];
-  node5 [label=" 1 0 1"];
-  node6 [label=" 1 1 0"];
-  node7 [label=" 1 1 1"];
-  node0 -> node0; 
-  node1 -> node1;
-  node2 -> node4;
-  node3 -> node5;
-  node4 -> node4;
-  node5 -> node5;
-  node6 -> node1;
-  node7 -> node2;
-  }"
+  deterministic_state_space = <<-EOS
+digraph test {
+node0 [label=" 0 0 0"];
+node1 [label=" 0 0 1"];
+node2 [label=" 0 1 0"];
+node3 [label=" 0 1 1"];
+node4 [label=" 1 0 0"];
+node5 [label=" 1 0 1"];
+node6 [label=" 1 1 0"];
+node7 [label=" 1 1 1"];
+node0 -> node0; 
+node1 -> node1;
+node2 -> node4;
+node3 -> node5;
+node4 -> node4;
+node5 -> node5;
+node6 -> node1;
+node7 -> node2;
+}
+    EOS
 
-  deterministic_state_space_with_probabilities = "digraph test {
-  node0 [label=" 0 0 0"];
-  node1 [label=" 0 0 1"];
-  node2 [label=" 0 1 0"];
-  node3 [label=" 0 1 1"];
-  node4 [label=" 1 0 0"];
-  node5 [label=" 1 0 1"];
-  node6 [label=" 1 1 0"];
-  node7 [label=" 1 1 1"];
-  node0 -> node0 [label= "1.00"];
-  node1 -> node1 [label= "1.00"];
-  node2 -> node4 [label= "1.00"];
-  node3 -> node5 [label= "1.00"];
-  node4 -> node4 [label= "1.00"];
-  node5 -> node5 [label= "1.00"];
-  node6 -> node1 [label= "1.00"];
-  node7 -> node2 [label= "1.00"];
-  }"
-
+  deterministic_state_space_with_probabilities = <<-EOS
+digraph test {
+node0 [label=" 0 0 0"];
+node1 [label=" 0 0 1"];
+node2 [label=" 0 1 0"];
+node3 [label=" 0 1 1"];
+node4 [label=" 1 0 0"];
+node5 [label=" 1 0 1"];
+node6 [label=" 1 1 0"];
+node7 [label=" 1 1 1"];
+node0 -> node0 [label= "1.00"];
+node1 -> node1 [label= "1.00"];
+node2 -> node4 [label= "1.00"];
+node3 -> node5 [label= "1.00"];
+node4 -> node4 [label= "1.00"];
+node5 -> node5 [label= "1.00"];
+node6 -> node1 [label= "1.00"];
+node7 -> node2 [label= "1.00"];
+}
+    EOS
 
   stochastic_function_file = ""
-
 
   test "creates wiring diagram" do
   end
