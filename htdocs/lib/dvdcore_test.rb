90fd4ba6fdb359d753b8c6751675d32be1de7474
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-10-13 22:08:24 -0400

    skeleton test file for dvdcore.rb

---
+++ htdocs/lib/dvdcore_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class DvdcoreTest < ActiveSupport::TestCase
+  test "dummy" do 
+    assert true
+  end
+
+  test "creates wiring diagram" do
+  end
+
+  test "check deterministic wiring diagram" do 
+  end
+
+  test "check stochastic wiring diagram" do 
+  end
+
+  test "creates state space" do
+  end
+
+  test "check deterministic state space" do 
+  end
+
+  test "check stochastic state space" do 
+  end
+
+end
