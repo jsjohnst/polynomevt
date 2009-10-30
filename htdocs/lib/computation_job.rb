2349b21bfd5558b7109cfc7027b327c98ee45c1d
Jeremy Johnstone <jstone@air-33j-net.local>
2009-10-30 16:29:46 -0700

    Fixed computation job to pass macaulay it's logger and to globally handle exceptions

--- htdocs/lib/computation_job.rb
+++ htdocs/lib/computation_job.rb
@@ -7,152 +7,156 @@ require 'react'
 require 'macaulay'
 
 class ComputationJob < Struct.new(:job_id)  
-  def perform  
-    # here is where we will handle the background Macaulay processing  
-    # and simulation of the network
-    
-    # first we go fetch the job so we know what we need to do
-    @job = Job.find(job_id)
-    
-    # now we setup a unique path to this job's data
-    @job.file_prefix = 'files/files-' + Digest::MD5.hexdigest( @job.id.to_s )
-   
-    # save input data TODO read from file
-    datafile = "public/" + @job.file_prefix + ".input.txt"
-    File.open(datafile, 'w') {|file| file.write(@job.input_data) }
+  def perform
+    begin  
+			# here is where we will handle the background Macaulay processing  
+			# and simulation of the network
+			
+			# first we go fetch the job so we know what we need to do
+			@job = Job.find(job_id)
+			
+			# now we setup a unique path to this job's data
+			@job.file_prefix = 'files/files-' + Digest::MD5.hexdigest( @job.id.to_s )
+		 
+			# save input data TODO read from file
+			datafile = "public/" + @job.file_prefix + ".input.txt"
+			File.open(datafile, 'w') {|file| file.write(@job.input_data) }
 
-    discretized_file = "public/" + @job.file_prefix + ".discretized_input.txt"
-    
-    @logger = Logger.new(File.join(RAILS_ROOT, 'log', 'computation_job.log'))
-    @logger.info "Discretized_file => " + discretized_file
-   
-    # clean file of extra white spaces before discretizing
-    File.open(datafile, 'r') do |file|
-      output  = File.open("public/" + @job.file_prefix + ".tmp.txt", 'w') 
-      while line = file.gets
-        my_array = line.split
-        first = true
-        my_array.each do |number|
-          puts number
-          if first
-            output.print number
-            first = false
-          else
-            output.print " " + number
-          end
-        end
-        output.print "\n"
-      end
-      output.close
-    end
-    File.copy("public/" + @job.file_prefix + ".tmp.txt", datafile)
+			discretized_file = "public/" + @job.file_prefix + ".discretized_input.txt"
+			
+			@logger = Logger.new(File.join(RAILS_ROOT, 'log', 'computation_job.log'))
+			Macaulay.logger = @logger    
 
-    # discretize files
-    @logger.info "pwd => " + Dir.getwd
-    
-    Macaulay.run("Discretize.m2", "discretize(///#{File.join(RAILS_ROOT, datafile)}///, 0, ///#{File.join(RAILS_ROOT, discretized_file)}///)")
-    
-    n_react_threshold = 5;
-    generate_picture = false
-    
-    if @job.show_wiring_diagram || @job.show_functions
-      wiring_diagram_dotfile = "public/" + @job.file_prefix + ".wiring_diagram.dot"
-      wiring_diagram_graphfile = "public/" + @job.file_prefix + ".wiring_diagram." + @job.wiring_diagram_format
-      state_space_dotfile = "public/" + @job.file_prefix + ".state_space.dot"
-      state_space_graphfile = "public/" + @job.file_prefix + ".state_space." + @job.state_space_format
-      functionfile = "public/" + @job.file_prefix + ".functionfile.txt"
-      consistent_datafile = "public/" + @job.file_prefix + ".consistent_input.txt"
-      
-      if @job.show_wiring_diagram && !@job.show_functions
-        if @job.nodes <= n_react_threshold
-          if !Macaulay.run("isConsistent.m2", "isConsistent(///#{File.join(RAILS_ROOT, discretized_file)}///, #{@job.pvalue}, #{@job.nodes})", true)
-            react = React.new(File.join(RAILS_ROOT, 'public', @job.file_prefix), @job.nodes)
-            react.discretized_data_file = discretized_file
-            begin
-              unless react.run 
-                @logger.info "React failed, aborting"
-                self.abort() 
-              end
-            rescue 
-              @logger.info "React threw exception, aborting"
-              self.abort()
-            end
-            generate_picture = true
-          else
-            # TODO
-            @logger.info "judging from the flow chart we're supposed to use
-            wd, but wd has not been rewritten to use a file with hashes
-            instead of a list"
-            Macaulay.run("wd.m2", "wd(///#{File.join(RAILS_ROOT, discretized_file)}///, ///#{File.join(RAILS_ROOT, wiring_diagram_dotfile)}///, #{@job.pvalue}, #{@job.nodes})")
-            `dot -T #{@job.wiring_diagram_format} -o #{wiring_diagram_graphfile} #{wiring_diagram_dotfile}`
-          end
-        else
-          self.check_and_make_consistent(datafile, consistent_datafile, discretized_file)
-          @logger.info "running minsetsWD"
-          Macaulay.run("minsets-web.m2", "minsetsWD(///#{File.join(RAILS_ROOT, discretized_file)}///, ///#{File.join(RAILS_ROOT, wiring_diagram_dotfile)}///, #{@job.pvalue}, #{@job.nodes})")
-          `dot -T #{@job.wiring_diagram_format} -o #{wiring_diagram_graphfile} #{wiring_diagram_dotfile}`
-        end
-      else
-        if @job.make_deterministic_model
-          @logger.info "deterministic"
-          if @job.nodes <= n_react_threshold
-            react = React.new(File.join(RAILS_ROOT, 'public', @job.file_prefix), @job.nodes)
-            react.discretized_data_file = discretized_file
-            react.run
-            generate_picture = true
-          else
-            self.check_and_make_consistent(datafile, consistent_datafile, discretized_file)
-            @logger.info "running minsetsPDS"
-            Macaulay.run("minsets-web.m2", "minsetsPDS(///#{File.join(RAILS_ROOT, discretized_file)}///, ///#{File.join(RAILS_ROOT, functionfile)}///, #{@job.pvalue}, #{@job.nodes})")
-            generate_picture = true
-          end
-        else # stochastic
-          @logger.info "stochastic"
-          self.check_and_make_consistent(datafile, consistent_datafile, discretized_file)
-          generate_picture = true
-        end
-      end
-    end
-    
-    if generate_picture
-      @logger.info "Starting simulation of state space."
-      
-      @logger.info "Functionfile : " + functionfile
+			@logger.info "Discretized_file => " + discretized_file
+		 
+			# clean file of extra white spaces before discretizing
+			File.open(datafile, 'r') do |file|
+				output  = File.open("public/" + @job.file_prefix + ".tmp.txt", 'w') 
+				while line = file.gets
+					my_array = line.split
+					first = true
+					my_array.each do |number|
+						puts number
+						if first
+							output.print number
+							first = false
+						else
+							output.print " " + number
+						end
+					end
+					output.print "\n"
+				end
+				output.close
+			end
+			File.copy("public/" + @job.file_prefix + ".tmp.txt", datafile)
 
-      unless File.exists?(functionfile) 
-        @logger.info "Functionfile has not been written so we can't run dvd_stochastic_runner on it. "
-        self.abort()
-      end
+			# discretize files
+			@logger.info "pwd => " + Dir.getwd
+			
+			Macaulay.run("Discretize.m2", "discretize(///#{File.join(RAILS_ROOT, datafile)}///, 0, ///#{File.join(RAILS_ROOT, discretized_file)}///)")
+			
+			n_react_threshold = 5;
+			generate_picture = false
+			
+			if @job.show_wiring_diagram || @job.show_functions
+				wiring_diagram_dotfile = "public/" + @job.file_prefix + ".wiring_diagram.dot"
+				wiring_diagram_graphfile = "public/" + @job.file_prefix + ".wiring_diagram." + @job.wiring_diagram_format
+				state_space_dotfile = "public/" + @job.file_prefix + ".state_space.dot"
+				state_space_graphfile = "public/" + @job.file_prefix + ".state_space." + @job.state_space_format
+				functionfile = "public/" + @job.file_prefix + ".functionfile.txt"
+				consistent_datafile = "public/" + @job.file_prefix + ".consistent_input.txt"
+				
+				if @job.show_wiring_diagram && !@job.show_functions
+					if @job.nodes <= n_react_threshold
+						if !Macaulay.run("isConsistent.m2", "isConsistent(///#{File.join(RAILS_ROOT, discretized_file)}///, #{@job.pvalue}, #{@job.nodes})", true)
+							react = React.new(File.join(RAILS_ROOT, 'public', @job.file_prefix), @job.nodes)
+							react.discretized_data_file = discretized_file
+							unless react.run 
+								@logger.info "React failed, aborting"
+								self.abort() 
+							end
+							generate_picture = true
+						else
+							# TODO
+							@logger.info "judging from the flow chart we're supposed to use
+							wd, but wd has not been rewritten to use a file with hashes
+							instead of a list"
+							Macaulay.run("wd.m2", "wd(///#{File.join(RAILS_ROOT, discretized_file)}///, ///#{File.join(RAILS_ROOT, wiring_diagram_dotfile)}///, #{@job.pvalue}, #{@job.nodes})")
+							`dot -T #{@job.wiring_diagram_format} -o #{wiring_diagram_graphfile} #{wiring_diagram_dotfile}`
+						end
+					else
+						self.check_and_make_consistent(datafile, consistent_datafile, discretized_file)
+						@logger.info "running minsetsWD"
+						Macaulay.run("minsets-web.m2", "minsetsWD(///#{File.join(RAILS_ROOT, discretized_file)}///, ///#{File.join(RAILS_ROOT, wiring_diagram_dotfile)}///, #{@job.pvalue}, #{@job.nodes})")
+						`dot -T #{@job.wiring_diagram_format} -o #{wiring_diagram_graphfile} #{wiring_diagram_dotfile}`
+					end
+				else
+					if @job.make_deterministic_model
+						@logger.info "deterministic"
+						if @job.nodes <= n_react_threshold
+							react = React.new(File.join(RAILS_ROOT, 'public', @job.file_prefix), @job.nodes)
+							react.discretized_data_file = discretized_file
+							react.run
+							generate_picture = true
+						else
+							self.check_and_make_consistent(datafile, consistent_datafile, discretized_file)
+							@logger.info "running minsetsPDS"
+							Macaulay.run("minsets-web.m2", "minsetsPDS(///#{File.join(RAILS_ROOT, discretized_file)}///, ///#{File.join(RAILS_ROOT, functionfile)}///, #{@job.pvalue}, #{@job.nodes})")
+							generate_picture = true
+						end
+					else # stochastic
+						@logger.info "stochastic"
+						self.check_and_make_consistent(datafile, consistent_datafile, discretized_file)
+						generate_picture = true
+					end
+				end
 
-      dvd = DVDCore.new(File.join(RAILS_ROOT, 'public', @job.file_prefix), @job.nodes, @job.pvalue)
-      dvd.create_wiring_diagram = @job.show_wiring_diagram
-      dvd.create_state_space = @job.show_state_space
-      dvd.show_probabilities = @job.show_probabilities_state_space
-      dvd.run
+			end
+			
+			if generate_picture
+				@logger.info "Starting simulation of state space."
+				
+				@logger.info "Functionfile : " + functionfile
 
-      simulation_output = "Fixed points: #{dvd.fixed_points}"
-      @logger.info simulation_output
-      
-      if @job.show_wiring_diagram
-        unless File.exists?(wiring_diagram_dotfile)
-          @logger.info "Wiring diagram dotfile has not been written."
-          self.abort() 
-        end
-        `dot -T #{@job.wiring_diagram_format} -o #{File.join(RAILS_ROOT, wiring_diagram_graphfile)} #{File.join(RAILS_ROOT, wiring_diagram_dotfile)}`  
-      end
+				unless File.exists?(functionfile) 
+					@logger.info "Functionfile has not been written so we can't run dvd_stochastic_runner on it. "
+					self.abort()
+				end
 
-      if @job.show_state_space
-        unless File.exists?(state_space_dotfile)
-          @logger.info "Wiring diagram dotfile has not been written."
-          self.abort() 
-        end
-        `dot -T #{@job.state_space_format} -o #{File.join(RAILS_ROOT, state_space_graphfile)} #{File.join(RAILS_ROOT, state_space_dotfile)}`  
-      end
+				dvd = DVDCore.new(File.join(RAILS_ROOT, 'public', @job.file_prefix), @job.nodes, @job.pvalue)
+				dvd.create_wiring_diagram = @job.show_wiring_diagram
+				dvd.create_state_space = @job.show_state_space
+				dvd.show_probabilities = @job.show_probabilities_state_space
+				dvd.run
 
-    end
-      
-    # we succeeded if we got to here!
-    self.success()
+				simulation_output = "Fixed points: #{dvd.fixed_points}"
+				@logger.info simulation_output
+				
+				if @job.show_wiring_diagram
+					unless File.exists?(wiring_diagram_dotfile)
+						@logger.info "Wiring diagram dotfile has not been written."
+						self.abort() 
+					end
+					`dot -T #{@job.wiring_diagram_format} -o #{File.join(RAILS_ROOT, wiring_diagram_graphfile)} #{File.join(RAILS_ROOT, wiring_diagram_dotfile)}`  
+				end
+
+				if @job.show_state_space
+					unless File.exists?(state_space_dotfile)
+						@logger.info "Wiring diagram dotfile has not been written."
+						self.abort() 
+					end
+					`dot -T #{@job.state_space_format} -o #{File.join(RAILS_ROOT, state_space_graphfile)} #{File.join(RAILS_ROOT, state_space_dotfile)}`  
+				end
+			end
+				
+			# we succeeded if we got to here!
+			self.success()
+	
+  	rescue => exc 
+			@logger.info "Something threw an exception, aborting"
+			self.abort()
+			raise exc
+		end
   end  
   
   def check_and_make_consistent(datafile, consistent_datafile, discretized_file)
