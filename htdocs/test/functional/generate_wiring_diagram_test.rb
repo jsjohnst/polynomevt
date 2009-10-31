c7bd8ed33b7749ce5ea16095c89fc261b027084b
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-10-31 00:15:58 -0400

    add test for gfan wiring diagram

--- htdocs/test/functional/generate_wiring_diagram_test.rb
+++ htdocs/test/functional/generate_wiring_diagram_test.rb
@@ -51,19 +51,32 @@ class GenerateWdTest < ActiveSupport::TestCase
     consistent_data_file = file_prefix + ".consistent.txt"
     create_file(consistent_data_file, consistent_data) 
     GenerateWiringDiagram.run_minsets(consistent_data_file, "#{file_prefix}.minset_wd.dot")
+    assert File.exists?("#{file_prefix}.minset_wd.dot")
+
     inconsistent_data_file = file_prefix + ".inconsistent.txt"
     create_file( inconsistent_data_file, inconsistent_data) 
     assert_raise MacaulayError do 
-      GenerateWiringDiagram.run_minsets(inconsistent_data_file, "#{file_prefix}.minset_wd.dot" )
+      GenerateWiringDiagram.run_minsets(inconsistent_data_file, "#{file_prefix}.dummy_dw.dot" )
     end
   end
 
   test "generate wd with gfan" do 
-    #wd.me
+    file_prefix = "/tmp/xxxwd"
+		@fake_job.file_prefix = file_prefix
+    Algorithm.job = @fake_job
+    
+    consistent_data_file = file_prefix + ".consistent.txt"
+    create_file(consistent_data_file, consistent_data) 
+    GenerateWiringDiagram.run_gfan(consistent_data_file, "#{file_prefix}.gfan_wd.dot")
+    assert File.exists?("#{file_prefix}.gfan_wd.dot")
+
+    inconsistent_data_file = file_prefix + ".inconsistent.txt"
+    create_file( inconsistent_data_file, inconsistent_data) 
+    assert_raise MacaulayError do 
+      GenerateWiringDiagram.run_gfan(inconsistent_data_file, "#{file_prefix}.dummy_wd.dot" )
+    end
   end
 
-  test "generate wd with react" do 
-  end
 end
 
 class FakeJob < Struct.new(:pvalue, :nodes, :file_prefix) 
