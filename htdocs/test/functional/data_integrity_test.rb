dd4126b5d56a42c72a2dda75a2e4387907383426
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-10-30 22:24:28 -0400

    add test for make consistent when inconsistent file has hash symbols

--- htdocs/test/functional/data_integrity_test.rb
+++ htdocs/test/functional/data_integrity_test.rb
@@ -41,25 +41,6 @@ class DataIntegrityTest < ActiveSupport::TestCase
 0 0 1
      EOS
 
-  def create_file( absolute_path, content )
-    File.open(absolute_path, "w") do |f|
-      f.write(content)
-    end
-  end
-  
-  def compare_content(file_name, expected_data)
-    # check that the file has at least as many lines as the dummy data, so
-    # that we don't generate empty graphs anymore
-    number_of_lines = `wc -l < #{file_name}`
-    assert expected_data.length <= number_of_lines.to_i, "data should have at least #{expected_data.length} lines, but only has #{number_of_lines}"
-    my_file = File.open( file_name, "r")
-    expected_data.each_with_index do |data, line|
-      file_data = my_file.gets
-      file_data = file_data.chop
-      assert file_data.include?( data ), "#{data} is not included in #{file_data}, line #{line}"
-    end
-  end
-
   test "basic consistent test" do
     file_prefix = "/tmp/xxxcons"
     Macaulay.pvalue = 3
@@ -107,6 +88,27 @@ class DataIntegrityTest < ActiveSupport::TestCase
 "0 1 0"
     ]
     compare_content(new_consistent_data_file, expected_data)
+  end
+  
+  test "basic make consistent with hash symbols test" do
+    file_prefix = "/tmp/xxxcons"
+    Macaulay.pvalue = 2 
+    Macaulay.nodes = 3
+    
+    inconsistent_with_hash_data_file = file_prefix + ".inconsistent_with_hash.txt"
+    create_file( inconsistent_with_hash_data_file, inconsistent_with_hash_data)
+    assert !DataIntegrity.consistent_with_hash?(inconsistent_with_hash_data_file)
+    new_consistent_with_hash_data_file = file_prefix + ".new_consistent_with_hash.txt"
+    DataIntegrity.makeConsistent(inconsistent_with_hash_data_file, new_consistent_with_hash_data_file )
+    expected_data = [
+"#TS1",
+"0 1 0",
+"0 0 1",
+"#TS2",
+"1 0 1",
+"0 1 0"
+    ]
+    compare_content(new_consistent_with_hash_data_file, expected_data)
   end
 
 end
