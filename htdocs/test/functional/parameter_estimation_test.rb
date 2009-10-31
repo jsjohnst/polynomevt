cfccf05351c19c7706bd387b1dd5ac764296cc7f
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-10-31 01:00:04 -0400

    another test, both results hand approved

--- htdocs/test/functional/parameter_estimation_test.rb
+++ htdocs/test/functional/parameter_estimation_test.rb
@@ -91,6 +91,48 @@ class ParameterEstimationTest < ActiveSupport::TestCase
     compare_content("#{file_prefix}.minsetwith_hash.txt", expected_data)
   end
 
+  test "generate function file with gfan" do 
+    file_prefix = "/tmp/xxxff"
+		@fake_job.file_prefix = file_prefix
+    Algorithm.job = @fake_job
+    
+    # checked by hand and is correct
+    consistent_data_file = file_prefix + ".consistent.txt"
+    create_file(consistent_data_file, consistent_data) 
+    ParameterEstimation.run_gfan(consistent_data_file, "#{file_prefix}.gfan.txt")
+    assert File.exists?("#{file_prefix}.gfan.txt")
+    expected_data = [
+"f1 = x1+x2+1",
+"f2 = x1",
+"f3 = x1+1"
+    ]
+    compare_content("#{file_prefix}.gfan.txt", expected_data)
+
+    inconsistent_data_file = file_prefix + ".inconsistent.txt"
+    create_file( inconsistent_data_file, inconsistent_data) 
+    assert_raise MacaulayError do 
+      GenerateWiringDiagram.run_gfan(inconsistent_data_file, "#{file_prefix}.dummy.txt" )
+    end
+  end
+
+  test "generate function file with gfan witih hash symbols" do 
+    file_prefix = "/tmp/xxxff2"
+		@fake_job.file_prefix = file_prefix
+    Algorithm.job = @fake_job
+    
+    # checked by hand and is correct
+    consistent_data_file = file_prefix + ".consistentwith_hash.txt"
+    create_file(consistent_data_file, consistent_with_hash_data) 
+    ParameterEstimation.run_gfan(consistent_data_file, "#{file_prefix}.gfanwith_hash.txt")
+    assert File.exists?("#{file_prefix}.gfanwith_hash.txt")
+    expected_data = [
+"f1 = x1*x2+x2*x3+x1+x3",
+"f2 = x1",
+"f3 = x1*x2+x2*x3+x2"
+    ]
+    compare_content("#{file_prefix}.gfanwith_hash.txt", expected_data)
+  end
+
 
 end
 
