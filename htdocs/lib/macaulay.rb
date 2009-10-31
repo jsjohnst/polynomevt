d0d824f655b033d689e068f4b4a83dcc94f6d553
Jeremy Johnstone <jstone@air-33j-net.local>
2009-10-30 17:31:13 -0700

    Don't require a logger anymore

--- htdocs/lib/macaulay.rb
+++ htdocs/lib/macaulay.rb
@@ -10,10 +10,10 @@ class Macaulay
   # check the return value of a command normally (ie isConsistent) but
   # in most cases we want to just exit on m2 failure
   def self.run(m2_file, m2_command, continue_on_error = false)
-    @logger.info "cd ../macaulay/; M2 #{m2_file} --stop --no-debug --silent -q -e \"#{m2_command}; exit 0;\"; cd ../htdocs;"
-    @logger.info `cd ../macaulay/; M2 #{m2_file} --stop --no-debug --silent -q -e "#{m2_command}; exit 0;"; cd ../htdocs;`
+    @logger.info "cd ../macaulay/; M2 #{m2_file} --stop --no-debug --silent -q -e \"#{m2_command}; exit 0;\"; cd ../htdocs;" unless @logger.nil?
+    @logger.info `cd ../macaulay/; M2 #{m2_file} --stop --no-debug --silent -q -e "#{m2_command}; exit 0;"; cd ../htdocs;` unless @logger.nil?
     if continue_on_error && $? != 0
-       @logger.info "Macaulay (#{m2_file}) (#{m2_command}) returned a non-zero exit code (#{$?}), aborting."
+       @logger.info "Macaulay (#{m2_file}) (#{m2_command}) returned a non-zero exit code (#{$?}), aborting." unless @logger.nil?
        raise MacaulayError.new "Macaulay (#{m2_file}) (#{m2_command}) returned a non-zero exit code (#{$?}), aborting."
     end
     $? == 0
