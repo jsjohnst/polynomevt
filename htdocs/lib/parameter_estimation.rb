bbda8ba87e584170c8ea5598d261cb719db930c4
Franziska B Hinkelmann <fhinkel@vt.edu>
2009-11-06 23:16:43 -0500

    run new gfan version

--- htdocs/lib/parameter_estimation.rb
+++ htdocs/lib/parameter_estimation.rb
@@ -20,7 +20,7 @@ class ParameterEstimation < Algorithm
 	end
 
   def self.run_gfan(discretized_file, functionfile)
-		self.run_macaulay("sgfan.m2", "sgfan(///#{discretized_file}///, ///#{functionfile}///, #{Algorithm.job.pvalue}, #{Algorithm.job.nodes})")
+		self.run_macaulay("sgfan.m2", "sgfan( (///#{discretized_file}///,null), ///#{functionfile}///, #{Algorithm.job.pvalue}, #{Algorithm.job.nodes})")
   end
 
 	def self.run_minsets(discretized_file, functionfile)	
